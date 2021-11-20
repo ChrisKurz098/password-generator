be2a441b97e6abf479f8a04e62464d8aced5ed1d
CKURZ <ckurz098@gmail.com>
2021-11-20 15:53:22 -0800

    added full list of special characters

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ function generatePassword() {
    var lowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
    var upperArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
    var numberArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-   var symbolArray = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "=", "_", "+", "<", ">", "?", "/", ";", ":", "[", "]"];
+   var symbolArray = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "=", "_", "+", "<", ">", "?", "/", ";", ":", "[", "]","{","}","`","~"];
  
    /////////add the components of each set of characters based off prev selections///////
    var characterArray = []; //this will hold all of the characters that were chosen by user
@@ -63,7 +63,7 @@ function generatePassword() {
  
    console.log(characterArray)
    
-  /////////////actually generat code//////////
+  /////////////actually generate code//////////
   var generatedPassword = "";
 
   for (i = 0; i < passwordLength; i++) {
