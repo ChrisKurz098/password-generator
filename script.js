7ea8052f80a16ee77aa97ffa622a24076d6ae7e3
CKURZ <ckurz098@gmail.com>
2021-11-20 15:35:42 -0800

    Added length prompt with check

--- script.js
+++ script.js
@@ -2,8 +2,19 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  passwordLength=8
- var characterArray = ['a','b','c','d','e','f','g']
+  var min=8;
+  var max=128;
+  passwordLength='';
+  askLength();
+  function askLength() {
+    passwordLength = window.prompt("Password Length?");
+    
+    if (passwordLength < min || passwordLength > max || passwordLength === "" ) {
+      window.alert("Please enter a number between 8-128");
+      askLength();
+    }
+  }
+ var characterArray = ['a','b','c','d','e','f','g'];
   /////////////actually generat code//////////
   var generatedPassword = "";
 
