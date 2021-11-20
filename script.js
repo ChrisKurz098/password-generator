90e352b11a9a25480fd74ff169ce59df731ba182
CKURZ <ckurz098@gmail.com>
2021-11-20 15:48:46 -0800

    creates an array of characters based of the users chosen character types

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   var min=8;
   var max=128;
-  passwordLength='';
+  var passwordLength='';
+
+  //ask for number, if it is <8 or >128 ask again
   askLength();
   function askLength() {
     passwordLength = window.prompt("Password Length?");
@@ -14,8 +16,8 @@ function generatePassword() {
       askLength();
     }
   }
-  
-   //must define these outside of function fist so we can use them pitside the function
+  //Ask user for charater types. if they dont choose any character types then ask again
+   //must define these outside of function fist so we can use them outside the function
    var useUpper = false;
    var useLower = false;
    var useNumbers = false;
@@ -31,8 +33,36 @@ function generatePassword() {
        getTypes();
      }
    }
-   debugger;
- var characterArray = ['a','b','c','d','e','f','g'];
+
+   //array that holds the character type selection
+   var chosenTypes = [useLower, useUpper, useNumbers, useSymbols];
+   console.log(chosenTypes);
+ 
+   //differnt character types saved in arrays by type
+   var lowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+   var upperArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+   var numberArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+   var symbolArray = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "=", "_", "+", "<", ">", "?", "/", ";", ":", "[", "]"];
+ 
+   /////////add the components of each set of characters based off prev selections///////
+   var characterArray = []; //this will hold all of the characters that were chosen by user
+
+   //checks each value in the array. if its true it adds the corrosponding array to characterArray set
+   if (chosenTypes[0]) {
+     characterArray = characterArray.concat(lowerArray);
+   }
+   if (chosenTypes[1]) {
+     characterArray = characterArray.concat(upperArray);
+   }
+   if (chosenTypes[2]) {
+     characterArray = characterArray.concat(numberArray);
+   }
+   if (chosenTypes[3]) {
+     characterArray = characterArray.concat(symbolArray);
+   }
+ 
+   console.log(characterArray)
+   
   /////////////actually generat code//////////
   var generatedPassword = "";
 
